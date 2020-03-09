603817b60b5df10129ccd072c18dff9f7b59a5cf
Kushagra Raina <kushagra.raina@gmail.com>
2020-03-09 15:12:38 +0530

    testing oneshot accuracy

---
+++ test_one_shot.py
@@ -0,0 +1,14 @@
+def test_oneshot(model, N, k, s="val", verbose=0):
+    """Test average N way oneshot learning accuracy of a siamese neural net over k one-shot tasks"""
+    n_correct = 0
+    if verbose:
+        print("Evaluating model on {} random {} way one-shot learning tasks ... \n".format(k, N))
+    for i in range(k):
+        inputs, targets = make_oneshot_task(N, s)
+        probs = model.predict(inputs)
+        if np.argmax(probs) == np.argmax(targets):
+            n_correct += 1
+    percent_correct = (100.0 * n_correct / k)
+    if verbose:
+        print("Got an average of {}% {} way one-shot learning accuracy \n".format(percent_correct, N))
+    return percent_correct

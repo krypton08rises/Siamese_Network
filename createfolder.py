e7beaddf2f91d074716b8423f74b901b9963e082
Kushagra Raina <kushagra.raina@gmail.com>
2020-03-20 17:22:54 +0530

    creates distinct folders for each foot

--- createfolder.py
+++ createfolder.py
@@ -6,8 +6,6 @@ for feet in os.listdir(r"./Images_evaluation/"):
     image_path = os.path.join(r"./Images_evaluation/", feet)
     img = Image.open(image_path)
     print(feet + feet[0:9])
-    if path.isdir(r"./Images_in_folder/" + feet[0:9]):
-        img.save(r"./Images_in_folder/" + feet[0:9] + "/" + feet)
-    else:
+    if not path.isdir(r"./Images_in_folder/" + feet[0:9]):
         os.makedirs(r"./Images_in_folder/" + feet[0:9])
-        img.save(r"./Images__in_folder/" + feet[0:9] + "/" + feet)
+    img.save(r"./Images_in_folder/" + feet[0:9] + "/" + feet)
